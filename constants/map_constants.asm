beb8800b796f29d099e8ead3d8bc4fa8ee517bf4
OldHarmony <falco.mackowiak+github@gmail.com>
2023-03-12 03:46:34 +0100

    Resize the map of route 13 for more space so that we can insert a little way left up to shortcut the labyrint

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -42,7 +42,7 @@ DEF FIRST_ROUTE_MAP EQU const_value
 	map_const ROUTE_10,                      10, 36 ; $15
 	map_const ROUTE_11,                      30,  9 ; $16
 	map_const ROUTE_12,                      10, 54 ; $17
-	map_const ROUTE_13,                      30,  9 ; $18
+	map_const ROUTE_13,                      30, 10 ; $18
 	map_const ROUTE_14,                      10, 27 ; $19
 	map_const ROUTE_15,                      30,  9 ; $1A
 	map_const ROUTE_16,                      20,  9 ; $1B
