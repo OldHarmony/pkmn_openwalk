8877f60c76b4e914176725cecbc3f2304e1caff4
OldHarmony <83431840+OldHarmony@users.noreply.github.com>
2024-04-10 01:24:30 +0200

    the new undergroundpath is now correct connected to the game

--- data/maps/objects/UndergroundPathPalletTown.asm
+++ data/maps/objects/UndergroundPathPalletTown.asm
@@ -2,8 +2,8 @@ UndergroundPathPalletTown_Object:
 	db $a ; border block
 
 	def_warp_events
-	warp_event  3,  7, LAST_MAP, 4
-	warp_event  4,  7, LAST_MAP, 4
+	warp_event  3,  7, PALLET_TOWN, 4
+	warp_event  4,  7, PALLET_TOWN, 4
 	warp_event  4,  4, UNDERGROUND_PATH_PALLETTOWN_VIRIDIANCITY, 2
 
 	def_bg_events
