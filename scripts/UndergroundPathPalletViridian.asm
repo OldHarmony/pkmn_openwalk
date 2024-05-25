f599ca531d583190a81567ee8674f597783a85aa
OldHarmony <83431840+OldHarmony@users.noreply.github.com>
2024-05-25 22:59:25 +0200

    rocket guy stop now the player in undround of viridian

--- scripts/UndergroundPathPalletViridian.asm
+++ scripts/UndergroundPathPalletViridian.asm
@@ -6,7 +6,13 @@ UndergroundPathPalletViridian_Script:
 
 UndergroundPathPalletViridian_ScriptPointers:
 	def_script_pointers
-	dw_const UndergroundPathPalletViridianDefaultScript, SCRIPT_UNDERGROUNDPATHPALLETVIRIDIAN_DEFAULT
+	dw_const UndergroundPathPalletViridianDefaultScript,                SCRIPT_UNDERGROUNDPATHPALLETVIRIDIAN_DEFAULT
+	dw_const UndergroundPathPalletViridianRocketGuyWalksToPlayerScript, SCRIPT_UNDERGROUNDPATHPALLETVIRIDIAN_ROCKETGUY_WALKS_TO_PLAYER
+	dw_const UndergroundPathPalletViridianRocketGuyTalkToPlayerScript,  SCRIPT_UNDERGROUNDPATHPALLETVIRIDIAN_ROCKETGUY_TALK_TO_PLAYER
+	dw_const UndergroundPathPalletViridianDisableScript,                SCRIPT_UNDERGROUNDPATHPALLETVIRIDIAN_NOOP
+
+UndergroundPathPalletViridianDisableScript:
+	ret
 
 UndergroundPathPalletViridianDefaultScript:
 	ld a, [wPartyCount]
@@ -15,21 +21,72 @@ UndergroundPathPalletViridianDefaultScript:
 	ld hl, UndergroundPathPalletViridian_RocketGuy_sees_Player_Coords
 	call ArePlayerCoordsInArray
 	ret nc
+	ld a, SFX_STOP_ALL_MUSIC
+	call PlaySound
+	ld a, BANK(Music_MeetEvilTrainer)
+	ld c, a
+	ld a, MUSIC_MEET_EVIL_TRAINER
+	call PlayMusic
+	xor a
+	ld [wEmotionBubbleSpriteIndex], a ; player's sprite
+	ld [wWhichEmotionBubble], a ; EXCLAMATION_BUBBLE
+	predef EmotionBubble
 	ld a, TEXT_UNDERGROUNDPATHPALLETVIRIDIAN_ROCKETGUY_NOPKMN
 	ldh [hSpriteIndexOrTextID], a
-	jp DisplayTextID
+	call DisplayTextID
+	ld a, SCRIPT_UNDERGROUNDPATHPALLETVIRIDIAN_ROCKETGUY_WALKS_TO_PLAYER
+	ld [wUndergroundPathPalletViridianCurScript], a
+	ret
 
 UndergroundPathPalletViridian_RocketGuy_sees_Player_Coords:
-if DEF(_DEBUG)
 	dbmapcoord 2, 10
 	dbmapcoord 3, 10
 	dbmapcoord 4, 10
 	dbmapcoord 5, 10
-endc
 	dbmapcoord 5, 11
 	dbmapcoord 5, 12
 	dbmapcoord 5, 13
 	db -1 ; end
+
+UndergroundPathPalletViridianRocketGuyWalksToPlayerScript:
+	ld a, SELECT | START | D_RIGHT | D_LEFT | D_UP | D_DOWN
+	ld [wJoyIgnore], a
+
+	ld a, $1
+	ldh [hNPCPlayerRelativePosPerspective], a
+	ld a, $1
+	swap a
+	ldh [hNPCSpriteOffset], a
+	predef CalcPositionOfPlayerRelativeToNPC
+	ldh a, [hNPCPlayerYDistance]
+	dec a
+	ldh [hNPCPlayerYDistance], a
+	predef FindPathToPlayer
+	ld de, wNPCMovementDirections2
+	ld a, UNDERGROUNDPATHPALLETVIRIDIAN_ROCKETGUY
+	ldh [hSpriteIndex], a
+	call MoveSprite
+
+	ld a, SCRIPT_UNDERGROUNDPATHPALLETVIRIDIAN_ROCKETGUY_TALK_TO_PLAYER
+	ld [wUndergroundPathPalletViridianCurScript], a
+	ret
+
+UndergroundPathPalletViridianRocketGuyTalkToPlayerScript:
+	; waiting for rocket guy is ready to walk to player
+	ld a, [wd730]
+	bit 0, a
+	ret nz
+
+	xor a
+	ld [wJoyIgnore], a
+	call PlayDefaultMusic ; reset to map music
+	ld a, TEXT_UNDERGROUNDPATHPALLETVIRIDIAN_ROCKETGUY
+	ldh [hSpriteIndexOrTextID], a
+	call DisplayTextID
+
+	ld a, SCRIPT_UNDERGROUNDPATHPALLETVIRIDIAN_NOOP
+	ld [wUndergroundPathPalletViridianCurScript], a
+	ret
 
 UndergroundPathPalletViridian_TextPointers:
 	def_text_pointers
