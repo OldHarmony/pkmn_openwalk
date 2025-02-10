9b0447d067ff76cd627320be2fa7b8c747379853
Wrutschgeluck <3431840+OldHarmony@users.noreply.github.com>
2025-02-10 15:14:41 +0100

    Corrected the spelling for the Rocketguy in the underground from Pallet to Virdiana City

--- text/UndergroundPathPalletViridian.asm
+++ text/UndergroundPathPalletViridian.asm
@@ -1,40 +1,42 @@
 _UndergroundPathPalletViridian_RocketGuyText1::
-    text "Hey der Boss hat"
+    text "Hey, der Boss hat"
     line "gesagt, hier darf"
     cont "niemand durch!"
     done
 
 _UndergroundPathPalletViridian_RocketGuyText2::
-    text "Ich habe ja so"
-	line "kein Bock mehr,"
-	cont "auf das hier"
-	cont "...."
+    text "Ich habe ja sowas"
+	line "von keinen Bock"
+	cont "mehr auf das"
+	cont "Ganz hier ...."
 	
 	para "Hey du!"
-	para "Ja du!"
+	para "Ja, du!"
 	
-	para "Ich habe hier so"
-	line "nen Vieh. Ich"
-	cont "glaub zwar nicht,"
-	cont "dass es ein"
-	cont "#MON ist, auch"
-	cont "weil mein #DEX"
-	cont "keine Infos hat."
+	para "Ich habe hier"
+	line "so'nen Vieh. Ich"
+	cont "glaube aber"
+	cont "nicht, dass es"
+	cont "ein #MON ist."
+	cont "Auch, weil mein"
+	cont "#DEX keine"
+	cont "Infos hat."
 
 	para "Hey, wenn du es"
 	line "mir abkaufst,"
 	cont "kann ich hier"
 	cont "endlich weg!"
 	cont "Willst du es für"
-	cont "¥@"
+	cont "nur ¥@"
 	text_bcd wPlayerMoney, 3 | LEADING_ZEROES | LEFT_ALIGN
 	text " haben?"
 	done
 
 _UndergroundPathPalletViridian_RocketGuyText3::
-	text "Wenn das so ist."
+	text "Also wenn das"
+	line "so ist."
 
-	para "Verschwinde und"
-	line "spiel wo anders"
-	cont "kleiner."
+	para "Dann verschwinde"
+	line "und spiel"
+	cont "woanders, Kleiner."
 	done
